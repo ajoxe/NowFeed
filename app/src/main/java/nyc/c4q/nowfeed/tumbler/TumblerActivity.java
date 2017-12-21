3136314c0b38a3856400885c1b95a01cfbe02c39
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2017-12-20 23:29:08 -0500

    added packages for apis

---
+++ app/src/main/java/nyc/c4q/nowfeed/tumbler/TumblerActivity.java
@@ -0,0 +1,15 @@
+package nyc.c4q.nowfeed.tumbler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nyc.c4q.nowfeed.R;
+
+public class TumblerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tumbler);
+    }
+}
